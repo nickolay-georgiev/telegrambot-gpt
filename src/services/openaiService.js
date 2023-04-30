be2e2131993a2f3e58124fe46efc2a87794d1909
Nikolay GeorgievN <nickwork5500@gmail.com>
2023-04-30 20:06:18 +0300

    Connect to the gtp api. Add logic to transcript messages and functionality to chat with the bot

---
+++ src/services/openaiService.js
@@ -0,0 +1,38 @@
+import config from 'config'
+import { createReadStream } from 'fs'
+import { Configuration, OpenAIApi } from 'openai'
+
+class OpenAIService{
+    constructor(apiKey){
+        const configuration = new Configuration({ apiKey });
+        this.openAI = new OpenAIApi(configuration);
+        this.roles = { Assistant: 'assistant', User: 'user', System: 'system' }
+    }
+
+    async chatWithOpenAI(messages) {
+        try {
+            const chatResponse = await this.openAI.createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages
+            })
+            return chatResponse.data.choices[0].message
+        } catch (error) {
+            console.log('Error on send message')
+        }
+    }
+
+    async transcriptMessage(file) {
+        try {
+            const transcriptionResponse = await this.openAI.createTranscription(
+                createReadStream(file),
+                'whisper-1'
+            )
+            return transcriptionResponse.data.text
+        } catch (error) {
+            console.log('Error on message transcription')
+        }
+    }
+
+}
+
+export const openAIService = new OpenAIService(config.get('OPENAI_API_KEY'))
